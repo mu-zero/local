98c69156c632eba829c4589b36ce3081586cb33b
Ashwin Dewan <ashwin.k.dewan@gmail.com>
2016-01-03 13:18:03 -0800

    everything but socket.io client address

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var DirecTV = require('directv-remote');
 var groveSensor = require('jsupm_grove');
 var request = require('request');
-//var socket = require('socket.io-client')('http://localhost');
+var socket = require('socket.io-client')('http://localhost');
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
@@ -19,36 +19,16 @@ var cabtimer =null;
 var sitting = 0;
 var tookmeds = 0;
 var couchtimer = null;
-var first = 1;
-var onphone =0;
+
 
 
 // Read the input and print both the raw value and a rough lux value,
 // waiting one second between readings
 function readLightSensorValue() {
     if(light.value() < 3) {
-        if(onphone ==0) {
-             Remote.processKey('play');
-        }
-       
+     sitting =1;
     }
-    else  Remote.processKey('pause');
-}
-
-function presence() {
-     request('https://graph.api.smartthings.com/api/smartapps/installations/5c4c9922-153e-4eb2-a0c0-18677fbb7faf/switches/c4052ba8-8aec-42ea-94ec-b8c6cdb33afb',
- function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-        var obj = JSON.parse(body);
-        if(obj.command == 'on') {
-            onphone =1;
-            Remote.processKey('pause');
-        }  
-        else{
-            onphone =0;
-        }
-  }
- }).auth(null, null, true, '0373663b-9c6c-4f7e-af8a-8658cdbc352e');
+    else sitting = 0;
 }
 
 
@@ -60,16 +40,8 @@ function cabinet() {
         if(obj.contact == 'open') {
             tookmeds = 1;
             Remote.webStop();
-            if(first ==1) {
-                Remote.play('1');
-                first =0;
-                clearInterval(cabtimer);
-            }
-            
-        }  
-        else{
-            
-        }
+            clearInterval(cabtimer);
+        }      
   }
  }).auth(null, null, true, '0373663b-9c6c-4f7e-af8a-8658cdbc352e');
 }
@@ -77,39 +49,35 @@ function cabinet() {
 function defaultMedia(person) {
     if(sitting == 1) {
          if(person=='jack') {
-           if(tookmeds ==1) {
-               Remote.webStop();   
-               Remote.tune('206');
+           if(tookmeds ==1) { 
+               Remote.play('6');
            }
-           else Remote.webStart ('urltoostaticpage');            
+           else Remote.webStart('http://10.10.30.40:8080/meds.html');          
          }
          else{
              Remote.play('10');
          }
-         clearInterval(couchtimer);
     }
 }
 
 connect().use(serveStatic(__dirname)).listen(8080);
 
-setInterval(readLightSensorValue, 500);
+setInterval(readLightSensorValue, 250);
 cabtimer = setInterval(cabinet, 2000);
-setInterval(lamp, 2000);
-Remote.webStart('http://10.10.30.40:8080/meds.html');  
 
-  //socket.on('connect', function(){console.log('connected')});
-  //socket.on('presence', function(data){
-    //  var presence = JSON.parse(data);
-      //couchtimer = setInterval(defaultMedia(presence.person),500);
-  //});
-  //socket.on('phone', function(data){
-    // if(sitting == 1) Remote.processKey('pause');
-  //});
-  //socket.on('speech', function(){
-    //  Remote.play('1');
-  //});
-  
- // connect().use(serveStatic(__dirname)).listen(8080);
+
+  socket.on('connect', function(){console.log('connected')});
+  socket.on('presence', function(data){
+      var presence = JSON.parse(data);
+      couchtimer = setInterval(defaultMedia(presence.person),500);
+  });
+  socket.on('phone', function(data){
+    if(sitting == 1) Remote.processKey('pause');
+  });
+  socket.on('speech', function(){
+    Remote.play('1');
+  });
+
   
   
   //socket.on('disconnect', function(){});
